bf64e38267174fc0d60d99a8c512be5a098190e0
Mahavir Ganapati Dash <mahavir.s.dr@gmail.com>
2026-05-27 10:09:45 +0530

    feat: functions and control flow

---
+++ 02-functions-and-control-flow/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    // println!("Hello, world!");
+    // let sum = add(3,2);
+
+
+
+    // //simple if statement
+    // let x = 5;
+    // // only if
+    // if x < 5 {
+    //     println!("x is less than 5");
+    // }
+
+
+
+    // // if and else both
+    // if x < 5 {
+    //     println!("x is less than 5");
+    // } else {
+    //     println!("x is greater than or equal to 5");
+    // }
+
+
+    // // if and else chain
+    // if x < 5 {
+    //     println!("x is less than 5");
+    // } else if x == 5 {
+    //     println!("x is equal to 5");
+    // } else  if x>5{
+    //     println!("x is greater than 5");
+    // }
+
+
+    // // if as an expression
+    // let y = if x<5 {0} else {1};
+
+
+    //match
+    //loops
+}
+
+
+//writw a custom function
+fn add(a: i32, b:i32) -> i32 {
+
+    // early return 
+    if a>5 {
+        return 0;
+    }
+    
+     a+b
+}
+
